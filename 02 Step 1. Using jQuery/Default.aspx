71441d498e5cf1ecdf32aa123bf9a3f329de1f74
CatBayun <catbayun@gmail.com>
2014-10-02 12:05:24 +0300

    [jQuery] Добавлена библиотека, начато её изучение

---
+++ 02 Step 1. Using jQuery/Default.aspx
@@ -0,0 +1,34 @@
+﻿<%@ Page Language="C#" 
+    AutoEventWireup="true" 
+    CodeBehind="Default.aspx.cs" 
+    Inherits="_02_Step_1.Using_jQuery.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Using jQuery</title>
+    <link rel="stylesheet" href="Styles.css" />
+    <script src="/Scripts/jquery-2.1.1.js"></script>
+    <script src="/Scripts/Default.js"></script>
+</head>
+<body>
+    <h2 id="summitsHeader">Summits</h2>
+    <table id="peaksTable">
+        <thead>
+            <tr>
+                <th class="name">Name</th>
+                <th class="height">Height (m)</th>
+            </tr>
+        </thead>
+        <tbody id="tableBody">
+            <tr><td class="name">Everest</td><td class="height">8848</td></tr>
+            <tr><td class="name">Aconcagua</td><td class="height">6962</td></tr>
+            <tr><td class="name">McKinley</td><td class="height">6194</td></tr>
+            <tr><td class="name">Kilimanjaro</td><td class="height">5895</td></tr>
+            <tr><td class="name">K2</td><td class="height">8611</td></tr>
+        </tbody>
+    </table>
+    <input type="button" value="Delete" id="deleteButton"/>
+</body>
+</html>
---
+++ 02 Step 1. Using jQuery/Scripts/Default.js
@@ -0,0 +1,43 @@
+﻿/// <reference path="jquery-2.1.1.js" />
+$(document).ready(function ()
+{
+    //$('th').addClass("higligth"); // выбрать элементы th
+    //$('h2').addClass("higligth2"); // выбрать элементы h2
+    //$('.name').addClass("higligth3"); // выбрать классы name
+    //$('#deleteButton').addClass("higligth1"); // выбрать элементы с id = "deleteButton"
+
+    //$('h2').addClass("higligth2"); // выбрать элементы h2
+    //$('h2 + table > thead > tr > th').addClass("higligth");
+    //$('h2 + table > tbody > tr > td').addClass("higligth1");
+
+    $('table td:first').addClass("higligth1");
+    $('table td:last').addClass("higligth1");
+
+    //$('tr:eq(1)').addClass("higligth");
+
+    //$('table td:eq(5)').addClass("higligth1");
+
+    //$('tr:has(td:contains("Kin")) > td').addClass("higligth2");
+    $(':button').addClass("higligth2");
+    $('tr:has(td:contains("Kili"))').css("color", "blue");
+    $('table').find("td[class]").parent().filter(":even").css("color", "red");
+
+    $('tr').prepend("<td></td>");
+    $('<input name="delete" type="radio"/>').prependTo("tbody td:first-child").first().attr("checked", true);
+
+    $('td').bind("mouseenter", function (e)
+    {
+        e.addClass("higligth1");
+    });
+
+    $('td').bind("mouseleave", function (e)
+    {
+        e.removeClass("higligth1");
+    });
+
+    $(":button").bind("click", function (e)
+    {
+        $(':radio:checked').closest('tr').remove();
+        $(':radio').first().attr("checked", true);
+    });
+});
